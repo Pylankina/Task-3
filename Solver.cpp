39d15945300f79addbd6f83cab444e35b925087a
Pylankina <126517247+Pylankina@users.noreply.github.com>
2024-09-06 17:38:08 +0300

    Solver.cpp

--- Solver.cpp
+++ Solver.cpp
@@ -12,7 +12,7 @@ bool Solver::graphColorUtil(int m, int r) {
         if (r == v) 
             return true;
         for (int c = 1; c <= m; c++) 
-            if (isSafe(r, c)) {
+            if (cheking(r, c)) {
                 colour[r] = c; // Присваиваем цвет
                 if (graphColorUtil(m, r + 1)) // Пытаемся раскрасить последующие вершины
                     return true;
@@ -21,7 +21,7 @@ bool Solver::graphColorUtil(int m, int r) {
         return false;
 }
 
-bool Solver::isSafe(int r, int c) {
+bool Solver::cheking(int r, int c) {
     for (int i = 0; i < graph[r].size(); i++)
         if (graph[r][i] == 1 && colour[i] == c) // Проверяем только соседей
             return false;
